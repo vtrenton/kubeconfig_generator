e9841652fe7593ac8a6024337007fea98ec3d023
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-12-15 22:33:54 -0600

    moved main to better readibility
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- cmd/kcgen/kcgen.go
+++ cmd/kcgen/kcgen.go
@@ -141,53 +141,6 @@ func (kuser kubeuser) genKubeconfig(kubeclient *kubernetes.Clientset) (*api.Conf
 	return kubeconfig, nil
 }
 
-func main() {
-	var kuser kubeuser
-	kubeclient := genkubeclient()
-
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: pass a path to a config yaml file as an argument to this program to generate a Kubeconfig for a user and the assoiated Rolebindings/ClusterRoleBindings for the user on the cluster itself.")
-		fmt.Println("Please make sure you have a valid admin kubeconfig at $HOME/.kube/config as well")
-		os.Exit(0)
-	}
-
-	if !fileExists(os.Args[1]) {
-		log.Fatalf("Invalid config file")
-	}
-
-	// configuration file exist set it here
-	configpath := os.Args[1]
-	kuser.parseConfigYaml(configpath)
-
-	// give the user a chance to stop before any changes are made
-	if validateCluster() {
-		kuser.createNewUser(kubeclient)
-		kubeconfig, err := kuser.genKubeconfig(kubeclient)
-		if err != nil {
-			log.Fatalf("Failed to generate kubeconfig: %v", err)
-		}
-
-		// Convert kubeconfig to YAML format
-		kubeconfigYAML, err := clientcmd.Write(*kubeconfig)
-		if err != nil {
-			log.Fatalf("Failed to convert kubeconfig to YAML: %v", err)
-		}
-
-		// Define the file path to write the kubeconfig file
-		kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", fmt.Sprintf("%s-kubeconfig.yaml", kuser.Username))
-
-		// Write the YAML data to a file
-		err = os.WriteFile(kubeconfigPath, kubeconfigYAML, 0644)
-		if err != nil {
-			log.Fatalf("Failed to write kubeconfig to file: %v", err)
-		}
-
-		log.Printf("Successfully wrote kubeconfig to %s", kubeconfigPath)
-	} else {
-		fmt.Println("No changes will be made!")
-	}
-}
-
 // painful helper function
 func fileExists(filename string) bool {
 	info, err := os.Stat(filename)
@@ -346,5 +299,52 @@ func createCRB(crb, username string, ctx context.Context, kubeclient *kubernetes
 		log.Printf("Failed to create cluster role binding for cluster role %s: %v", crb, err)
 	} else {
 		log.Printf("Created cluster role binding for cluster role %s", crb)
+	}
+}
+
+func main() {
+	var kuser kubeuser
+	kubeclient := genkubeclient()
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: pass a path to a config yaml file as an argument to this program to generate a Kubeconfig for a user and the assoiated Rolebindings/ClusterRoleBindings for the user on the cluster itself.")
+		fmt.Println("Please make sure you have a valid admin kubeconfig at $HOME/.kube/config as well")
+		os.Exit(0)
+	}
+
+	if !fileExists(os.Args[1]) {
+		log.Fatalf("Invalid config file")
+	}
+
+	// configuration file exist set it here
+	configpath := os.Args[1]
+	kuser.parseConfigYaml(configpath)
+
+	// give the user a chance to stop before any changes are made
+	if validateCluster() {
+		kuser.createNewUser(kubeclient)
+		kubeconfig, err := kuser.genKubeconfig(kubeclient)
+		if err != nil {
+			log.Fatalf("Failed to generate kubeconfig: %v", err)
+		}
+
+		// Convert kubeconfig to YAML format
+		kubeconfigYAML, err := clientcmd.Write(*kubeconfig)
+		if err != nil {
+			log.Fatalf("Failed to convert kubeconfig to YAML: %v", err)
+		}
+
+		// Define the file path to write the kubeconfig file
+		kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", fmt.Sprintf("%s-kubeconfig.yaml", kuser.Username))
+
+		// Write the YAML data to a file
+		err = os.WriteFile(kubeconfigPath, kubeconfigYAML, 0644)
+		if err != nil {
+			log.Fatalf("Failed to write kubeconfig to file: %v", err)
+		}
+
+		log.Printf("Successfully wrote kubeconfig to %s", kubeconfigPath)
+	} else {
+		fmt.Println("No changes will be made!")
 	}
 }
